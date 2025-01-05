81d081653546e09d88461a93472c24de4f97b46c
MahadAnsari18 <mahadansari18@gmail.com>
2025-01-06 02:19:17 +0500

    Answer1

---
+++ questionNo1.dart
@@ -0,0 +1,19 @@
+// Q.1: Write a program that takes a list of numbers as input and prints the
+// even numbers in the list using a for loop.
+// Example:
+// Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// Output: 2 4 6 8 10
+
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<int> evenNumbers = [];
+  for (int a in numbers) {
+    if (a % 2 == 0) {
+      evenNumbers.add(a);
+    }
+  }
+  print("Even numbers: $evenNumbers");
+}
+
+// Answer is ->
+//             Even numbers: [2, 4, 6, 8, 10]
