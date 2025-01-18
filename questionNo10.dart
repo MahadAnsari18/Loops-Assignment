638642984852d04c5a462903cbd54eca6789c8d0
MahadAnsari18 <mahadansari18@gmail.com>
2025-01-19 01:14:56 +0500

    Answer10

--- questionNo10.dart
+++ questionNo10.dart
@@ -1,17 +1,18 @@
-// Write a program to display the cube of the number up to an integer.
+// Q.10: Write a program to display the cube of the number up to an integer.
 // Test Data :
 // Input number of terms : 5
-// Expected Output :
-// Number is : 1 and cube of 1 is :1
-// Number is : 2 and cube of 2 is :8
-// Number is : 3 and cube of 3 is :27
-// Number is : 4 and cube of 4 is :64
-// Number is : 5 and cube of 5 is :125
 
 void main() {
   int number = 5;
   for (int i = 1; i <= number; i++) {
-    int cube = i*i*i;
+    int cube = i * i * i;
     print("Number is : $i and cube of $i is :$cube");
   }
 }
+
+// Answer is ->
+//             Number is : 1 and cube of 1 is :1
+//             Number is : 2 and cube of 2 is :8
+//             Number is : 3 and cube of 3 is :27
+//             Number is : 4 and cube of 4 is :64
+//             Number is : 5 and cube of 5 is :125
